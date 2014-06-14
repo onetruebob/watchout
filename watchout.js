1dce07a2f2dc22bf8f5b73f8b576d7f7d8f79e5b
Hack Reactor Students <bob@bobowen.org>
2014-06-13 17:27:41 -0700

    Player now 'draggable'.

--- watchout.js
+++ watchout.js
@@ -19,6 +19,102 @@ var gameBoard = d3.select('.container').append('svg:svg')
                 .attr('height', gameOptions.height);
 
 
+var Player = function (gameOptions) {
+  this._gameOptions = gameOptions;
+
+  this.path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
+
+  this.fill = '#ff6600';
+  this.x =  0;
+  this.y = 0;
+  this.angle = 0;
+  this.r = 5;
+
+
+
+};
+
+Player.prototype.render = function (to) {
+  this.el = to.append('svg:path')
+          .attr('d', this.path)
+          .attr('fill', this.fill);
+
+  this.transform ({
+    x: this._gameOptions.width * 0.5,
+    y: this._gameOptions.height * 0.5
+  });
+
+  this.setupDragging();
+};
+
+
+Player.prototype.getX = function(){
+  return this.x;
+};
+
+Player.prototype.setX = function (x){
+  var minX = this._gameOptions.padding;
+  var maxX = this._gameOptions.width - this._gameOptions.padding;
+  if(x <= minX) {
+    x = minX;
+  }
+  if(x >= maxX){
+    x = maxX;
+  }
+  this.x = x;
+};
+
+
+Player.prototype.getY = function(){
+  return this.y;
+};
+
+Player.prototype.setY = function (y){
+  var minY = this._gameOptions.padding;
+  var maxY = this._gameOptions.height - this._gameOptions.padding;
+  if (y <= minY){
+    y = minY;
+  }
+  if (y >= maxY) {
+    y = maxY;
+  }
+  this.y = y;
+};
+
+Player.prototype.transform = function(opts){
+  this.angle = opts.angle || this.angle;
+  this.setX(opts.x || this.x);
+  this.setY(opts.y || this.y);
+  this.el.attr("transform",
+      "rotate(" + this.angle + " " + this.getX() + " " + this.getY() + ") "+
+      "translate(" + this.getX() + " " + this.getY() + ")");
+};
+
+Player.prototype.moveAbsolute = function(x,y){
+  this.transform({x: x, y: y});
+};
+
+Player.prototype.moveRelative = function (dx, dy){
+  this.transform({
+    x: this.getX()+dx,
+    y: this.getY()+dy,
+    angle: 360 * (Math.atan2(dy,dx)/(Math.PI*2))
+  });
+};
+
+Player.prototype.setupDragging = function(){
+  var dragMove = function(){
+    this.moveRelative(d3.event.dx, d3.event.dy);
+  };
+  var drag = d3.behavior.drag()
+          .on('drag', dragMove.bind(this));
+  this.el.call(drag);
+};
+
+
+var players = [];
+players.push(new Player(gameOptions).render(gameBoard));
+
 var createEnemies = function () {
   return _.range(0,gameOptions.nEnemies).map(function (i) {
     return {
@@ -30,21 +126,21 @@ var createEnemies = function () {
 };
 
 
-var render = function (enemy_data) {
+var render = function (enemyData) {
 
   var enemies = gameBoard.selectAll('circle.enemy')
-    .data(enemy_data, function (d) { return d.id;});
+    .data(enemyData, function (d) { return d.id;});
 
   enemies.enter()
     .append('svg:circle')
       .attr('class', 'enemy')
-      .attr('cx', function (enemy){ return axes.x(enemy.x)})
-      .attr('cy', function (enemy){ return axes.y(enemy.y)})
+      .attr('cx', function (enemy){ return axes.x(enemy.x);})
+      .attr('cy', function (enemy){ return axes.y(enemy.y);})
       .attr('r', 10);
 
   enemies.attr('class', 'enemy')
-      .attr('cx', function (enemy){ return axes.x(enemy.x)})
-      .attr('cy', function (enemy){ return axes.y(enemy.y)})
+      .attr('cx', function (enemy){ return axes.x(enemy.x);})
+      .attr('cy', function (enemy){ return axes.y(enemy.y);});
 
   enemies.exit()
     .remove();
@@ -52,9 +148,10 @@ var render = function (enemy_data) {
 
 var play = function () {
   var gameTurn = function () {
-      var newEnemyPositions = createEnemies();
-      render(newEnemyPositions);
+    var newEnemyPositions = createEnemies();
+    render(newEnemyPositions);
   };
+
   gameTurn();
   /*increaseScore = ->
     gameStats.score += 1
